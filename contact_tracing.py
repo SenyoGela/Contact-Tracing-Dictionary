5eb31e2bfd0aaf1a821650aed6c1bb14ec3fa803
SenyoGela <senogelamariconb@gmail.com>
2022-11-13 00:50:18 +0800

    Add python file

---
+++ contact_tracing.py
@@ -0,0 +1,12 @@
+# display menu
+print("")
+print("********** MENU **********")
+print("(1) Add an item")
+print("(2) Search an item")
+print("(3) Exit")
+print("**************************")
+print("")
+# ask user to select item from the menu
+# if input is 1: add an item
+# if input is 2: search an item
+# if input is 3: exit
